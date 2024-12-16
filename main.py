ada91134af94d4649c79c1dadc1f3241a6c237f2
Tala2812 <guskovatala@gmail.com>
2024-12-16 14:38:03 +0300

    add new one greet

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 def greet():
-    return "Привет из главной ветки!"
+    return "Привет от Алисы!"
 
 if __name__ == "__main__":
     print(greet())
-
-
-    def greet():
-        return "Привет от Наты!"
-
-
-    if __name__ == "__main__":
-        print(greet())
